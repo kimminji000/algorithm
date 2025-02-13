c5e2cb180963e4aa3cd2555279b7836e02f2d147
minji <minji6764@naver.com>
2025-02-13 17:54:13 +0900

    250213

--- algorithm/src/swea/P5653.java
+++ algorithm/src/swea/P5653.java
@@ -10,19 +10,51 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class P5653 {
-	static int[][] graph;
-	static int[][] visited;
+	static int[][] graph; // 생명력
+	static int[][] hour; // 세포시간
+	static int n, m, k;
 
-	public static void bfs() {
-		Queue<int[]> queue = new LinkedList<int[]>();
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1 };
 
-		for (int i = 0; i < graph.length; i++) {
-			for (int j = 0; j < graph[i].length; j++) {
-				if (graph[i][j] != 0) {
-					queue.add(new int[] { i, j });
+	public static int bfs() {
+		for (int time = 1; time <= k; time++) { // k시간 동안
+
+			for (int i = 0; i < n + 400; i++) {
+				for (int j = 0; j < m + 400; j++) {
+					if (graph[i][j] != 0) { // 비어있지 않을때
+						hour[i][j]++; // 시간이 흐름
+
+					}
+				}
+			}
+
+			for (int i = 0; i < n + 400; i++) {
+				for (int j = 0; j < m + 400; j++) {
+
+					if (graph[i][j] != 0 && graph[i][j] + 1 == hour[i][j]) { // 번식 상태
+						for (int k = 0; k < 4; k++) {
+							if (hour[i + dx[k]][j + dy[k]] == 0) { // 세포시간이 0일때
+								graph[i + dx[k]][j + dy[k]] = Math.max(graph[i][j], graph[i + dx[k]][j + dy[k]]);
+							}
+						}
+					}
 				}
 			}
 		}
+
+		int cnt = 0;
+
+		for (int i = 0; i < n + 400; i++) {
+			for (int j = 0; j < m + 400; j++) {
+				if (graph[i][j] != 0 && hour[i][j] < 2 * graph[i][j]) { // 비어있지 않고 세포의 시간이 생명력의 2배미만
+					cnt++;
+				}
+			}
+		}
+
+		return cnt;
+
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -35,12 +67,12 @@ public class P5653 {
 		for (int i = 0; i < t; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
-			int n = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
-			int k = Integer.parseInt(st.nextToken());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			k = Integer.parseInt(st.nextToken());
 
 			graph = new int[n + 400][m + 400];
-			visited = new int[n + 400][m + 400];
+			hour = new int[n + 400][m + 400];
 
 			for (int x = 0; x < n; x++) {
 				st = new StringTokenizer(br.readLine());
@@ -49,9 +81,12 @@ public class P5653 {
 					graph[x + 200][y + 200] = Integer.parseInt(st.nextToken());
 				}
 			}
-		}
 
-		bfs();
+			int cnt = bfs();
+
+			sb.append("#").append(i + 1).append(" ").append(cnt).append("\n");
+
+		}
 
 		bw.write(sb.toString());
 		bw.flush();
