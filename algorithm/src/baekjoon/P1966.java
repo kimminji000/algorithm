5a1ef6e3f30b7904a2d95c0899f893ce45cb06e8
minji <minji6764@naver.com>
2025-02-18 17:56:51 +0900

    2025-02-18 백준/P1966 푸는 중

---
+++ algorithm/src/baekjoon/P1966.java
@@ -0,0 +1,39 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class P1966 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+
+			Queue<Integer> myQueue = new ArrayDeque<>();
+
+			for (int i = 0; i < m; i++) {
+				if(myQue)
+			}
+
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
---
+++ algorithm/src/baekjoon/P4949.java
@@ -0,0 +1,57 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+
+public class P4949 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		String str = br.readLine();
+
+		while (!str.equals(".")) {
+			ArrayDeque<Integer> myStack = new ArrayDeque<>();
+
+			boolean flag = false;
+
+			for (int i = 0; i < str.length(); i++) {
+				if (str.charAt(i) == '(') {
+					myStack.push(0);
+				} else if (str.charAt(i) == '[') {
+					myStack.push(1);
+				} else if (str.charAt(i) == ')') {
+					if (myStack.isEmpty() || myStack.peek() != 0) {
+						flag = true;
+						break;
+					}
+					myStack.pop();
+				} else if (str.charAt(i) == ']') {
+					if (myStack.isEmpty() || myStack.peek() != 1) {
+						flag = true;
+						break;
+					}
+					myStack.pop();
+				}
+			}
+
+			if (myStack.size() != 0 || flag) {
+				sb.append("no").append("\n");
+			} else {
+				sb.append("yes").append("\n");
+			}
+
+			str = br.readLine();
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
