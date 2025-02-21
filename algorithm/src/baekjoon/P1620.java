4a0b5a15c341299caf32eedad4793c358837c37a
SSAFY <SSAFY@DESKTOP-CQDJ23U>
2025-02-21 17:29:58 +0900

    2025-02-21 백준 P5215 푸는 중

---
+++ algorithm/src/baekjoon/P1620.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P1620 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < n; i++) {
+
+		}
+
+		for (int i = 0; i < m; i++) {
+
+		}
+
+		br.close();
+	}
+}
