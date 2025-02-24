a98ccf939640a7af2f14557fe48aa9557926b642
SSAFY <SSAFY@DESKTOP-CQDJ23U>
2025-02-24 16:20:33 +0900

    P1620 다시 풀어보기

--- algorithm/src/baekjoon/P1620.java
+++ algorithm/src/baekjoon/P1620.java
@@ -8,19 +8,30 @@ import java.util.StringTokenizer;
 public class P1620 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < n; i++) {
+		String[] pokemon = new String[n + 1];
 
+		for (int i = 1; i <= n; i++) {
+			pokemon[i] = br.readLine();
 		}
 
 		for (int i = 0; i < m; i++) {
+			String str = br.readLine();
 
+			if (str.charAt(0) >= '1' && str.charAt(0) <= '9') {
+				System.out.println(pokemon[Integer.parseInt(str)]);
+			} else {
+				for (int j = 1; j <= n; j++) {
+					if (pokemon[j].equals(str)) {
+						System.out.println(j);
+					}
+				}
+			}
 		}
 
 		br.close();
