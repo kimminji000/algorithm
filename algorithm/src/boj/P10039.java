72cd79604c0721eab18ec597dcd39b5a46dba948
kimminji000 <minji6764@naver.com>
2025-03-04 23:05:23 +0900

    boj/P10039

---
+++ algorithm/src/boj/P10039.java
@@ -0,0 +1,27 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P10039 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int sum = 0;
+
+		for (int i = 0; i < 5; i++) {
+			int score = Integer.parseInt(br.readLine());
+
+			if (score < 40) {
+				sum += 40;
+			} else {
+				sum += score;
+			}
+		}
+
+		System.out.println(sum / 5);
+
+		br.close();
+	}
+}
