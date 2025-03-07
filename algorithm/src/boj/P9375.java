270eadd5f42adf7a6fb6acc8c14165c336d40cda
kimminji000 <minji6764@naver.com>
2025-03-07 22:36:44 +0900

    boj/P9375

--- algorithm/src/boj/P9375.java
+++ algorithm/src/boj/P9375.java
@@ -3,63 +3,46 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class P9375 {
-	static List<String> category;
-	static int[] count;
-	static boolean[] subset;
-	static int cnt;
-
-	static void powerset(int start, int n) {
-		if (start == n) {
-			for (int i = 0; i < n; i++) {
-				if (subset[i]) {
-				}
-			}
-			return;
-		}
-
-		for (int i = 0; i < n; i++) {
-			subset[i] = true;
-			powerset(i + 1, n);
-
-			subset[i] = false;
-			powerset(i + 1, n);
-
-		}
-
-	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
 		int t = Integer.parseInt(br.readLine());
 
 		for (int i = 0; i < t; i++) {
 			int n = Integer.parseInt(br.readLine());
 
-			category = new ArrayList<>();
-			count = new int[n];
+			Map<String, Integer> category = new LinkedHashMap<>();
 
 			for (int j = 0; j < n; j++) {
-				br.readLine(); // name
-				String cate = br.readLine();
+				StringTokenizer st = new StringTokenizer(br.readLine());
 
-				if (category.contains(cate)) {
-					count[category.indexOf(cate)]++;
+				st.nextToken(); // name
+				String cate = st.nextToken();
+
+				if (category.containsKey(cate)) {
+					category.put(cate, category.get(cate) + 1);
 				} else {
-					category.add(cate);
-					count[category.indexOf(cate)]++;
+					category.put(cate, 1);
 				}
 			}
 
-			subset = new boolean[category.size()];
+			int cnt = 1;
 
-			powerset(0, category.size());
+			for (int j : category.values()) {
+				cnt *= (j + 1);
+			}
+
+			sb.append(cnt - 1).append("\n");
 
 		}
+
+		System.out.println(sb.toString());
 
 		br.close();
 	}
