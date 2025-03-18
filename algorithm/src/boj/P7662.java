07f96caaf9ded67049880bfd6a74b8e655c26664
SSAFY <SSAFY@DESKTOP-CQDJ23U>
2025-03-18 17:57:47 +0900

    boj/P7662

---
+++ algorithm/src/boj/P7662.java
@@ -0,0 +1,66 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class P7662 {
+	static class Number implements Comparable<Number> {
+		int index;
+		int num;
+
+		public Number(int index, int num) {
+			super();
+			this.index = index;
+			this.num = num;
+		}
+
+		@Override
+		public int compareTo(Number o) {
+			if (this.num == o.num) {
+				return this.index - o.index;
+			}
+			return this.num - o.num;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= t; tc++) {
+			TreeSet<Number> treeSet = new TreeSet<>();
+			int index = 0;
+
+			int k = Integer.parseInt(br.readLine());
+
+			for (int i = 0; i < k; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+
+				char alphbet = st.nextToken().charAt(0);
+				int n = Integer.parseInt(st.nextToken());
+
+				if (alphbet == 'I') {
+					treeSet.add(new Number(index++, n));
+				} else if (alphbet == 'D') {
+					if (!treeSet.isEmpty()) {
+						if (n == 1) {
+							treeSet.pollLast();
+						} else if (n == -1) {
+							treeSet.pollFirst();
+						}
+					}
+				}
+			}
+
+			if (treeSet.isEmpty()) {
+				System.out.println("EMPTY");
+			} else {
+				System.out.println(treeSet.last().num + " " + treeSet.first().num);
+			}
+		}
+	}
+}
