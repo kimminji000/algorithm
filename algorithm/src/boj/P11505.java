f2f647e2240190c870052bb0480d09bdc482a8b0
kimminji000 <minji6764@naver.com>
2025-03-20 22:46:22 +0900

    boj/P11505(푸는 중), P5522

--- algorithm/src/boj/P11505.java
+++ algorithm/src/boj/P11505.java
@@ -6,15 +6,23 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class P11505 {
-	static int[] num, tree;
+	static long[] num, tree;
 
-	static int init(int node, int start, int end) {
+	static long init(int node, int start, int end) {
 		if (start == end) {
 			return tree[node] = num[start];
 		}
 
 		int mid = (start + end) / 2;
-		return tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
+		return tree[node] = init(node * 2, start, mid) * init(node * 2 + 1, mid + 1, end) % 1000000007;
+
+	}
+
+	static long query() {
+		return 0;
+	}
+
+	static void update() {
 
 	}
 
@@ -27,13 +35,25 @@ public class P11505 {
 		int m = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 
-		num = new int[n + 1];
-		tree = new int[4 * n];
+		num = new long[n + 1];
+		tree = new long[4 * n];
 
 		for (int i = 1; i <= n; i++) {
 			num[i] = Integer.parseInt(br.readLine());
 		}
 
 		init(1, 1, n);
+
+		for (int i = 0; i < m + k; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+
+			if (a == 1) {
+//				update(1,1,n,)
+			}
+		}
 	}
 }
