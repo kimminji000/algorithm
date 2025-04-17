aef4c469ccb11e3fa7c3a2a3dd2c27acf33d5cfc
kimminji000 <minji6764@naver.com>
2025-04-17 17:56:34 +0900

    boj/P4485, P1987(푸는 중)

---
+++ algorithm/src/boj/P1987.java
@@ -0,0 +1,29 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P1987 {
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 0 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+
+		char[][] board = new char[r][c];
+
+		for (int i = 0; i < r; i++) {
+			String st = new 
+			for (int j = 0; j < c; j++) {
+
+			}
+		}
+	}
+}
