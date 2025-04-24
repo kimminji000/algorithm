60c93a8af2b715db53c62e10fc10bf5616014c29
kimminji000 <minji6764@naver.com>
2025-04-24 15:52:15 +0900

    boj/P9251(푸는 중), P12015

---
+++ algorithm/src/boj/P9251.java
@@ -0,0 +1,18 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P9251 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str1 = br.readLine();
+		String str2 = br.readLine();
+
+		int maxLen = 0;
+
+		System.out.println(maxLen);
+	}
+}
